c01c2a6e7c01222c7153dede3a5a4b6fd8bf18c1
Saurabh Gattani <saurabh.gattani@tal.com.au>
2016-11-08 10:02:22 +1100

    readme updated

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # reviews-component
 star rating reviews component using angularjs
+
+
+To run the code start webserver and goto http://localhost:8080/reviews-component/review.html
+
+For test automation, you need to have the following packages installed:
+
+1. npm install from command line in the project directory
+2. node.js (download and install https://nodejs.org/en/download/)
+3. webdriver-manager - npm update webdriver-manager if webdriver manager not already 
+installed or it prompts for andupdate,
+install using npm install webdriver-manager -g
+4. protractor - npm install  protactor -g
+
+
+To run test cases:-
+Go to the project root directory in command prompt, start selenium web 
+driver with the following command:- 
+
+=> webdriver-manager start
+
+Open another command prompt, go to the project root directory and run test cases 
+with the following command/s:-
+
+protactor tests/conf.js - this will run UI and js test cases
+by launching application in browser
--- tests/tests-spec.js
+++ tests/tests-spec.js
@@ -11,6 +11,7 @@ describe('Reviews component', function() {
 			element.all(by.css('cite.fn')).get(index).evaluate('reviews.username').then(function(name) {
 				 username = name;
 				 console.log(username, commentators[index]);
+				 // compare
 				 expect(username).toBe(commentators[index]);
 			});
 		}
@@ -19,110 +20,6 @@ describe('Reviews component', function() {
 
 
 /*
-describe('Run search test', function() {
-	it('Search Test1', function() {
-		console.log("Searching for Sean");
-		element(by.model('searchQuery')).sendKeys("Sean");
-		var people = element.all(by.repeater('user in filteredPeople'));
-		expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[0]);
-		element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test2', function() {
-	console.log("Searching for Yaw");
-	element(by.model('searchQuery')).sendKeys("Yaw");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[1]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test3', function() {
-	console.log("Searching for Lucy");
-	element(by.model('searchQuery')).sendKeys("Lucy");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[2]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test4', function() {
-	console.log("Searching for Eric");
-	element(by.model('searchQuery')).sendKeys("Eric");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[3]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test5', function() {
-	console.log("Searching for Rory");
-	element(by.model('searchQuery')).sendKeys("Rory");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[4]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test6', function() {
-	console.log("Searching for Hayley");
-	element(by.model('searchQuery')).sendKeys("Hayley");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[5]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test7', function() {
-	console.log("Searching for L");
-	element(by.model('searchQuery')).sendKeys("L");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[2]);
-	expect(people.get(1).element(by.css("#listName")).getText()).toMatch(usersList[5]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test8', function() {
-	console.log("Searching for Y");
-	element(by.model('searchQuery')).sendKeys("Y");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[1]);
-	expect(people.get(1).element(by.css("#listName")).getText()).toMatch(usersList[2]);
-	expect(people.get(2).element(by.css("#listName")).getText()).toMatch(usersList[4]);
-	expect(people.get(3).element(by.css("#listName")).getText()).toMatch(usersList[5]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test9', function() {
-	console.log("Searching for E");
-	element(by.model('searchQuery')).sendKeys("E");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[0]);
-	expect(people.get(1).element(by.css("#listName")).getText()).toMatch(usersList[3]);
-	expect(people.get(2).element(by.css("#listName")).getText()).toMatch(usersList[5]);
-	element(by.model('searchQuery')).clear();
-	});
-});
-
-describe('Run search test', function() {
-	it('Search Test10', function() {
-	console.log("Searching for R");
-	element(by.model('searchQuery')).sendKeys("R");
-	var people = element.all(by.repeater('user in filteredPeople'));
-	expect(people.get(0).element(by.css("#listName")).getText()).toMatch(usersList[3]);
-	expect(people.get(1).element(by.css("#listName")).getText()).toMatch(usersList[4]);
-	element(by.model('searchQuery')).clear();
-	});
-});
+	Note:- write more tests here to check number of reviews shown initially, expected behavior is met on button
+	clicks (show more/less) etc.
 */
